a20efaa21a6904ccabafad872b48ea655c7400ac
Ivo Matic <mivomable@gmail.com>
2018-12-13 16:24:07 +0100

    added static files, connected apps

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]+"..."
+
+    def shorttime(self):
+        return self.pub_date.strftime("%b %e %Y")
 # Create a blog model
 #       title, pub date, body, image
 
